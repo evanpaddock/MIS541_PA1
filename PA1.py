19c44a2e2111697a9ef4a25691fd35a35af0eddd
evanpaddock <edpaddock@crimson.ua.edu>
2023-08-31 22:23:29 -0500

    DrawShapes IP

--- PA1.py
+++ PA1.py
@@ -7,42 +7,59 @@ Created on Wed Aug 30 19:10:38 2023
 
 #Main Start
 def Main():
-    from time import sleep 
-    #Defining functions
-    #Shows main menu choices
+    #importing necessary items
+    
+    from time import sleep
+    import random
+    
+    #***************************************** Defining Functions Start******************************************
+    
+    #Shows main menu choices and get choice
     def MainMenu():
         print("Welcome to Danish's Game")
-        print("Please enter the number of the game you would like to play or '-1' to exit")
-        print("1: Rock Paper Scissors")
-        print("2: Draw shapes")
-        print("3: Currency Calculator")
+        print("Please enter the number of the game you would like to play")
+        print(" 1: Rock Paper Scissors")
+        print(" 2: Draw shapes")
+        print(" 3: Currency Calculator")
+        print("-1: exit")
         return input()
-    #
-    def IsNotValid(menu, choice):
+    
+    #Checks if the choice is valid for the given menu
+    def IsNotValid(choice):
         if(choice == "-1"):
             return
-        elif(menu == "Main"):
-            if(choice == "1"):
-                return
-            if(choice == "2"):
-                return
-            if(choice == "3"):
-                return
-            else:
-                return True
-    #
-    def RunChoice(menu, choice):
+        elif(choice == "1"):
+            return
+        elif(choice == "2"):
+            return
+        elif(choice == "3"):
+            return
+        else:
+            return True
+            
+    
+    #Main menu validation loop
+    def MenuValidLoop():
+        
+        choice = MainMenu()
+        
+        while IsNotValid(choice):
+            CLS()
+            print("Invalid option. Please choose again.\n")
+            choice = MainMenu()
+        return choice
+    
+    #Runs choice depending on menu
+    def RunChoice(choice):
+        CLS()
+        
         #Runs Main Menu Options
-        if(menu == "Main"):
-            if(choice == "1"):
-                # RPS()
-                print("RPS")
-            elif(choice == "2"):
-                # DrawShapes()
-                print("DrawShapes")
-            else:
-                #CurrencyCalculator()
-                print("CurrencyCalculator")
+        if(choice == "1"):
+            RPS()
+        elif(choice == "2"):
+            DrawShapes()
+        elif(choice == "3"):
+            CurrencyCalculator()
                 
     #Clears console
     def CLS():
@@ -50,37 +67,99 @@ def Main():
         
     #Rock Paper Scissors Game
     def RPS():
-        return
+        print("RPS")
+    
+    #Draws rectange by length and width and inverted right trianle by length
+    def DrawShapes():
+        
+        #Draw Shapes Game Main Menu
+        def MainMenu():
+            print("Please choose a shape!")
+            print(" 1. Rectangle")
+            print(" 2. Right Triangle")
+            print("-1. Go Back")
+            return input("Selection: ")
+        
+        #Draw Shapes Main Menu Validation Loop
+        def MenuValidLoop():
+            CLS()
+            
+            choice = MainMenu()
+            
+            while IsNotValid(choice):
+                print("Invalid option. Please choose again.\n")
+                choice = MainMenu()
+                
+            return choice
+        
+        def IsNotValid(choice):
+            if(choice == "-1"):
+                return
+            elif(choice == "1"):
+                return
+            elif(choice == "2"):
+                return
+            else:
+                return True
+        
+        def RunChoice(choice):
+            CLS()
+            
+            if(choice == "1"):
+                Rectangle()
+            elif(choice == "2"):
+                RightTriangle()
+        
+        def Rectangle():
+            print("Rectangle")
+            sleep(2)
+            
+        def RightTriangle():
+            print("Right Triangle")
+            sleep(2)
+        
+        choice = MenuValidLoop()
+        
+        while choice != "-1":
+            RunChoice(choice)
+            
+            choice = MenuValidLoop()
+            
+        Main()
+        
+    
+    #Shows currency by dollars to pennies
+    def CurrencyCalculator():
+        print("CurrencyCalculator")
+        
+    #***************************************** Defining Main Functions End ******************************************
+    
+
+    #***************************************** Main Function Start ******************************************
     
     CLS() 
     
-    choice = MainMenu()
-    
-    while IsNotValid("Main", choice):
-        CLS() 
-        
-        print("Invalid option. Please choose again.\n")
-        choice = MainMenu();
+    choice = MenuValidLoop()
         
     while choice != "-1":
         CLS() 
         
-        RunChoice("Main", choice)
+        RunChoice(choice)
         sleep(5)
         
         CLS()
         
-        choice = MainMenu()
+        choice = MenuValidLoop()
         
-        while IsNotValid("Main", choice):
-            CLS() 
-            
-            print("Invalid option. Please choose again.\n")
-            choice = MainMenu();
+    CLS()
+    print("Thank you for playing Danish's games! Bye Bye!")
+    sleep(3)
+    CLS()
         
+    #***************************************** Main Function End ******************************************
     
+#***************************************** Program Start *****************************************#      
     
-
 Main()
         
         
